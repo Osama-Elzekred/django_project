1723c6675066cd1c30261d4d7488a5609e7dcf39
Osama-Elzekred <osamaelzekred@gmail.com>
2021-08-18 15:50:34 +0200

    Django-project

---
+++ blog/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http  import HttpResponse
+
+posts=[
+
+{
+  'auther':'OsamaElzekred',
+  'title':'Blog post 1 ',
+  'content':'Frist post content',
+  'date_posted':'2021/8/17'
+},
+{
+  'auther':'SaraElzekred',
+  'title':'Blog post 2 ',
+  'content':'second post content',
+  'date_posted':'2021/8/18'
+}
+]
+
+def home(request):
+  context={
+    'posts': posts
+  }
+  return render(request,'blog/home.html',context)
+def About(request):
+  return render(request,'blog/about.html',{'title':'about'})
+
