44bc39e06b3cb24d69e77e3e271069377c1f2440
Osama-Elzekred <osamaelzekred@gmail.com>
2021-08-27 16:08:25 +0200

    - chenged post chape on home

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns=[
-  path('',views.home , name='Home-blog'),
-  path('about/',views.About, name='About-blog'),
+urlpatterns = [
+    path('', views.home, name='blog-home'),
+    path('about/', views.About, name='blog-about'),
 
-  ]
+]
--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,29 @@
 from django.shortcuts import render
-from django.http  import HttpResponse
+from django.http import HttpResponse
 
-posts=[
+posts = [
 
-{
-  'auther':'OsamaElzekred',
-  'title':'Blog post 1 ',
-  'content':'Frist post content',
-  'date_posted':'2021/8/17'
-},
-{
-  'auther':'SaraElzekred',
-  'title':'Blog post 2 ',
-  'content':'second post content',
-  'date_posted':'2021/8/18'
-}
+    {
+        'author': 'OsamaElzekred',
+        'title': 'Blog post 1 ',
+        'content': 'Frist post content',
+        'date_posted': '2021/8/17'
+    },
+    {
+        'author': 'SaraElzekred',
+        'title': 'Blog post 2 ',
+        'content': 'second post content',
+        'date_posted': '2021/8/18'
+    }
 ]
 
-def home(request):
-  context={
-    'posts': posts
-  }
-  return render(request,'blog/home.html',context)
-def About(request):
-  return render(request,'blog/about.html',{'title':'about'})
 
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def About(request):
+    return render(request, 'blog/about.html', {'title': 'about'})
