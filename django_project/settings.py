983f41dce4e3ee77cad0414ef2c411c2c55d613b
Osama-Elzekred <osamaelzekred@gmail.com>
2021-09-22 18:08:52 +0000

    last changes

--- django_project/settings.py
+++ django_project/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['172.105.73.177']
 
@@ -154,8 +154,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST= 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER =  config['EMAIL_USER']
-EMAIL_HOST_PASSWORD = config['EMAIL_PASS']
+EMAIL_HOST_USER =  'ericbruceking@gmail.com'
+EMAIL_HOST_PASSWORD = 'nqqtgsqcugwmmojt'
 
 
 
