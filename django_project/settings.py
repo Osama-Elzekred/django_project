4ca8dc73f8c35913b26ba4d997daa03b11f114ee
Osama-Elzekred <osamaelzekred@gmail.com>
2021-09-22 18:35:21 +0000

    adding config.get('EMAIL_USER')

--- django_project/settings.py
+++ django_project/settings.py
@@ -154,8 +154,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST= 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER =  'ericbruceking@gmail.com'
-EMAIL_HOST_PASSWORD = 'nqqtgsqcugwmmojt'
+EMAIL_HOST_USER =  config.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = config.get('EMAIL_PASS')
 
 
 
